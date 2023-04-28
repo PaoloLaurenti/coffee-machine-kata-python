a21963612a6b35334f4738b558aa1a5e5352ff0e
Paolo Laurenti <paolo.laurenti@prima.it>
2023-04-28 17:53:07 +0200

    refactoring

--- coffee_machine_kata/coffee_machine.py
+++ coffee_machine_kata/coffee_machine.py
@@ -6,6 +6,10 @@ class CoffeeMachine:
         self.drink_maker = drink_maker
 
     def dispense(self, beverage_request):
+        drink_maker_command = self.build_drink_maker_command(beverage_request)
+        self.drink_maker.run(drink_maker_command)
+
+    def build_drink_maker_command(self, beverage_request):
         drink_maker_beverage = ""
         match beverage_request.beverage_type:
             case BeverageType.TEA:
@@ -14,4 +18,5 @@ class CoffeeMachine:
                 drink_maker_beverage = "C"
             case BeverageType.CHOCOLATE:
                 drink_maker_beverage = "H"
-        self.drink_maker.run(f"{drink_maker_beverage}::")
+        drink_maker_command = f"{drink_maker_beverage}::"
+        return drink_maker_command
--- tests/test_coffee_machine.py
+++ tests/test_coffee_machine.py
@@ -1,38 +1,27 @@
+import pytest
 from coffee_machine_kata.beverage_request import BeverageRequest
 from coffee_machine_kata.beverage_type import BeverageType
 from coffee_machine_kata.coffee_machine import CoffeeMachine
 from tests.support.drink_maker_test_double import DrinkMakerTestDouble
 
+
 class TestCoffeeMachine:
-    def test_dispense_tea_no_sugar(self):
+    @pytest.mark.parametrize(
+        "beverage_type, expected_command",
+        [
+            (BeverageType.TEA, "T::"),
+            (BeverageType.COFFEE, "C::"),
+            (BeverageType.CHOCOLATE, "H::"),
+        ],
+    )
+    def test_dispense_beverage_no_sugar(self, beverage_type, expected_command):
         drink_maker_spy = DrinkMakerTestDouble()
         coffe_machine = CoffeeMachine(drink_maker_spy)
 
-        beverage_request = BeverageRequest(beverage_type=BeverageType.TEA, sugar=0)
+        beverage_request = BeverageRequest(beverage_type=beverage_type, sugar=0)
         coffe_machine.dispense(beverage_request)
 
         drink_maker_commands = drink_maker_spy.get_received_commands()
         assert len(drink_maker_commands) == 1
-        assert drink_maker_commands[0] == "T::"
+        assert drink_maker_commands[0] == expected_command
 
-    def test_dispense_chocolate_no_sugar(self):
-        drink_maker_spy = DrinkMakerTestDouble()
-        coffe_machine = CoffeeMachine(drink_maker_spy)
-
-        beverage_request = BeverageRequest(beverage_type=BeverageType.CHOCOLATE, sugar=0)
-        coffe_machine.dispense(beverage_request)
-
-        drink_maker_commands = drink_maker_spy.get_received_commands()
-        assert len(drink_maker_commands) == 1
-        assert drink_maker_commands[0] == "H::"
-
-    def test_dispense_coffee_no_sugar(self):
-        drink_maker_spy = DrinkMakerTestDouble()
-        coffe_machine = CoffeeMachine(drink_maker_spy)
-
-        beverage_request = BeverageRequest(beverage_type=BeverageType.COFFEE, sugar=0)
-        coffe_machine.dispense(beverage_request)
-
-        drink_maker_commands = drink_maker_spy.get_received_commands()
-        assert len(drink_maker_commands) == 1
-        assert drink_maker_commands[0] == "C::"
